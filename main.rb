8b67c1d1ed139059022f5b4bb51122ea90d3566d
silentscribe <seelive.i@gmail.com>
2019-02-19 19:17:16 +0000

    removed comment from main

--- main.rb
+++ main.rb
@@ -4,12 +4,3 @@ require './game'
 
 play = Game.new
 play.start_game
-
-# game_over
-#     switch_player
-#       if game_over == false  
-#         puts "P1: #{player1.score}/3 vs P2: #{player2.score}/3"
-#         puts "---- NEW TURN -----" 
-#       else
-#         puts "--- Game Over-----"
-#       end
