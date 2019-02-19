930840ff184999de674a1398194ce9c667f83013
silentscribe <seelive.i@gmail.com>
2019-02-19 03:11:22 +0000

    questions class written

--- game.rb
+++ game.rb
@@ -1,16 +1,26 @@
+require './player'
+require './questions'
+
 class Game
-  attr_accessor :player1, :player2
+  attr_accessor :player1, :player2, :current_player
 
   def initialize()
     @player1 = Player.new
     @player2 = Player.new
     @current_player = 1
+    @questions = Questions.new
   end
 
   def start_game()
     while player1.score > 0 || player2.score > 0
-      
-      puts ""
 
+      puts "Player #{current_player}: " 
+      response = $stdin.gets.chomp
+      
+    end
+  end
   
 end
+
+play = Game.new
+play.start_game
---
+++ questions.rb
@@ -0,0 +1,21 @@
+class Questions
+  attr_accessor :questions
+
+  def initialize()
+    @questions = { 
+      35 => 'What is 28 plus 7?',
+      22 => 'What is 11 plus 11?',
+      33 => 'What is 44 minus 11?',
+      17 => 'What is 10 plus 7?' 
+    }
+  end
+
+  def select_question
+    random_key = questions.keys.sample
+    questions.select { |k,v| k == random_key }
+  end
+
+end
+
+q = Questions.new
+q.select_question
